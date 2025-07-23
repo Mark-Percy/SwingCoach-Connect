a3d0ca81fdbc9cad72f89b09583ba5a374055c99
Mark Percy <mark.percy98@outlook.com>
2025-07-23 15:54:58 +0100

    added findAccounts by email and account status

--- backend/src/main/java/com/swingcoach/swingcoach_connect/repository/UserRepository.java
+++ backend/src/main/java/com/swingcoach/swingcoach_connect/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.swingcoach.swingcoach_connect.repository;
 
 import com.swingcoach.swingcoach_connect.model.User;
+import com.swingcoach.swingcoach_connect.model.User.AccountStatus;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByEmailStatus(String email, AccountStatus accountStatus);
 }
